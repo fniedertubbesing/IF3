25985c693db3384580e6cb5ef255d0010ed4f5f5
Ubuntu_16.04 <felix.niedertubbesing@hsbi.de>
2024-01-15 18:21:07 +0100

    client programmierung hinzugefügt

--- 03_DEVEL/gitReps/UNSER/client.C
+++ 03_DEVEL/gitReps/UNSER/client.C
@@ -10,19 +10,32 @@
 #include <unistd.h> //contains various constants
 
 #include "SIMPLESOCKET.H"
+#include "TASK1.H"
 
 using namespace std;
 
+class client : public TCPclient{
+private:
+    int pl_; // Passwortlänge
+    int al_; // Alphabetlänge
+public:
+    void set(); // Setzen der Passwort.- unf Alphabetlänge
+    client() : TCPclient(){};
+    string pwdguess(); // Methode zum erraten eines Passworts
+    string newpwd(); // Erzeugen eines Strings für den Server mit Passwort.- und Alphabetlänge
+
+};
+
 int main() {
 	srand(time(NULL));
-	TCPclient c;
+	client c;
 	string host = "localhost";
 	string msg;
 
 	//connect to host
 	c.conn(host , 2022);
 
-	int i=0;
+	/*int i=0;
 	bool goOn=1;
 	while(goOn){ // send and receive data
 		if((rand() % 20) < i++){
@@ -36,8 +49,53 @@ int main() {
 		msg = c.receive(32);
 		cout << "got response:" << msg << endl;
 		sleep(1);
+*/
 
+    c.set();
+    c.sendData(c.newpwd());
+    string imsg = c.receive(2);
+
+    int i;
+
+    if(imsg == "OK"){
+    imsg = "FALSE";
+    i = 0;
+
+    while(imsg == "FALSE"){
+    c.sendData(c.pwdguess());
+    imsg = c.receive(5);
+    i++;
+    }
+    }
+    else{cout << "Fehler" << endl;
+    }
+
+    cout << "Das Passwort wurde nach " << i << " Versuchen erraten";
+
+
+	return 0;
 	}
+
+
+string client::pwdguess(){
+    string pwdGuess = TASK1::SYMBOLS.substr(0,pl_);
+
+    for(int i = 0; i < pl_; i++){
+        pwdGuess[i] = TASK1::SYMBOLS[rand()% al_];
+    }
+    return pwdGuess;
 }
 
+void client::set(){
+cout << "Bitte geben Sie die Passwortlänge und die Alphabetlänge an:" << endl;
+cin >> pl_;
+cout << endl;
+cin >> al_ ;
+cout<< endl;
+return;
+}
+
+string client::newpwd(){
+return "NEWPWD[" + pl_ + ',' + al_ + ']';
+}
 
